f7af7ae10f8d6f542ed7fb9a3d1f80787eca6305
Degtyarev Vladimir <degtyarevvg@katren.ru>
2019-10-11 14:53:31 +0700

    Creating a "fade" transition with the CSS transition property

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,24 +2,40 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Built-in Directives</h1>
-        <p v-text="'Some Text'"></p>
-        <p v-html="'<strong>Strong Text</strong>'"></p>
-      </div>
-      <hr>
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <h1>Custom Directives</h1>
-        <p v-highlight:background.delayed="'green'">Color this</p>
-        <p v-highlight="'green'">Color this</p>
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
+  export default {
+  	data() {
+  		return {
+  			show: false
+      }
+    }
+  }
 </script>
 
 <style lang="scss">
+  .fade-enter {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+  .fade-leave {
 
+  }
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
 </style>
