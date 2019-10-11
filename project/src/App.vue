a25a047bfbc8004626c044d0f803bd5792c88721
Degtyarev Vladimir <degtyarevvg@katren.ru>
2019-10-11 15:01:40 +0700

    Creating a "slide" transition with the CSS transition property

--- project/src/App.vue
+++ project/src/App.vue
@@ -6,7 +6,7 @@
         <hr>
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br><br>
-        <transition name="fade">
+        <transition name="slide">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
       </div>
@@ -37,5 +37,35 @@
   .fade-leave-active {
     transition: opacity 1s;
     opacity: 0;
+  }
+
+  .slide-enter {
+
+  }
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+  .slide-leave {
+
+  }
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 </style>
